3b28a59ba524178585f67819627359f08e4c6404
LAKSHYA SHARMA <80743588+lucky478@users.noreply.github.com>
2023-09-22 11:33:45 +0530

    Time: 1 ms (96.99%), Space: 41.4 MB (90.37%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // function to check whether a number contains even digits or not
+     boolean even(int num) {
+        int numberOfDigits = digits(num);
+        
+        return numberOfDigits % 2 == 0;
+    }
+
+
+    // count number of digits in a number
+     int digits(int num) {
+
+        if (num < 0) {
+            num = num * -1;
+        }
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10; // num /= 10
+        }
+
+        return count;
+    }
+}
